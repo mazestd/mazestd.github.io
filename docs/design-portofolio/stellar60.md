b6fd9e657df2034dc816414c3a1fd1e62a3aa058
Han's <infint.one@gmail.com>
2023-12-17 11:02:39 +0700

    migrate to Docusaurus

---
+++ docs/design-portofolio/stellar60.md
@@ -0,0 +1,22 @@
+# Stellar60
+60% Keyboard PCB with Multi-layout Support
+
+## Introduction
+
+This Keyboard was one of commissioned design from our fellow designer. You would take a look into supported layout below.
+![Layout option](./img/stellar60_kle.png)
+
+## Technical Spesification
+
+- **Layout Size** : 60% Layouts with Split BS, Stepped-Caps, Split R-Shift and Tsangan Spacebar Support
+- **Compatible Switches** : MX Style Switches
+- **Micro-Controller** : Atmega32U4
+- **Connector** : SM04B JST Connector with multiple placement for keyboard support
+- **Hardware Protection** : Fused Power-Line
+- **Other** : Caps-Lock Indicator
+
+## Render & Prototype
+### Render
+![Stellar60](./img/stellar60_render.png)
+
+## Firmware & Software Information
---
+++ docusaurus.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Maze Studio',
+  tagline: 'Design, Build and Type!',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://mazestudio.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'mazestd', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Maze Studio',
+        logo: {
+          alt: 'Maze Studio',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'right',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'right'},
+          {
+            href: 'https://github.com/mazestd/docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'light',
+        copyright: `Copyright © ${new Date().getFullYear()} Maze Studio. Built with <a href="https://docusaurus.io/" >Docusaurus </a>`,
+      },
+      prism: {
+        theme: prismThemes.palenight,
+        darkTheme: prismThemes.palenight,
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+      },
+      algolia: {
+        appId: 'JIXR0QEQHU',
+        apiKey: '54078bc1e69761a74e8f6ce9d2c30968',
+        indexName: 'docs',
+        contextualSearch: true,
+      }
+    }),
+};
+
+export default config;
